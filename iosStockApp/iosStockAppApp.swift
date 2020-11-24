0fae864a139c1a2d85942df90d2a42245570af93
James Xu <54513773+fbjxu@users.noreply.github.com>
2020-11-24 11:58:22 -0800

    Initial Commit

---
+++ iosStockApp/iosStockAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosStockAppApp.swift
+//  iosStockApp
+//
+//  Created by Fangbo Xu on 11/24/20.
+//
+
+import SwiftUI
+
+@main
+struct iosStockAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
